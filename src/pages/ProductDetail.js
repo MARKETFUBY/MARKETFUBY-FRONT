96f1c011443ddf98cb135a12884a4ee501aad1f0
Yerineee <yerin1103@ewhain.net>
2023-11-01 16:10:33 +0900

    :construction: 제품 상세 페이지 작업 중

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Header from '../components/Common/Header';
+import ProductAtf from '../components/ProductDetail/ProductAtf';
+
+const ProductDetail = () => {
+    return (
+        <>
+            <Header />
+            <ProductAtf />
+        </>
+    );
+};
+
+export default ProductDetail;
