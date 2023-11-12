02253ea006da21668a66e27a3b242d62b6ceb515
Yerineee <yerin1103@ewhain.net>
2023-11-13 00:03:20 +0900

    :sparkles: 제품 상세 페이지 네브바 및 상품설명 컴포넌트 추가

---
+++ src/components/ProductDetail/NavBar.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+const NavBar = () => {
+    return (
+        <Wrapper>
+            <BtnWrapper>
+                <NavBtn>상품설명</NavBtn>
+                <NavBtn>상세정보</NavBtn>
+                <NavBtn>후기</NavBtn>
+                <NavBtn>문의</NavBtn>
+            </BtnWrapper>
+        </Wrapper>
+    );
+};
+
+export default NavBar;
+
+const Wrapper = styled.nav`
+    z-index: 20;
+    position: sticky;
+    top: 56px;
+    width: 1010px;
+    box-shadow: inset 0 -0.5px 0 0 #ddd;
+    background-color: #fff;
+    margin: 90px auto 0px;
+`;
+
+const BtnWrapper = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    height: 60px;
+    padding: 0;
+    list-style-type: none;
+
+    li {
+        &:last-child {
+            border-right-width: 1px;
+        }
+    }
+`;
+
+const NavBtn = styled.li`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    border-width: 1px 0 1px 1px;
+    border-color: #eee;
+    border-style: solid;
+    background-color: #fafafa;
+    cursor: pointer;
+`;
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 import Header from '../components/Common/Header';
 import ProductAtf from '../components/ProductDetail/ProductAtf';
+import NavBar from '../components/ProductDetail/NavBar';
+import ProductDescription from '../components/ProductDetail/ProductDescription';
 
 const ProductDetail = () => {
     return (
         <>
             <Header />
             <ProductAtf />
+            <NavBar />
+            <ProductDescription />
         </>
     );
 };
